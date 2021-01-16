53f995b7f7daba2adfcebcfed524d77d8969e3e4
“Tanika <tanikajangam03@gmail.com>
2021-01-16 14:24:15 -0500

    added voice recognition

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {useState, useEffect} from 'react';
 import './App.css';
 import LearnMore from "./images/learn-more.svg"
@@ -7,13 +8,48 @@ import Mission from "./images/mission-img.svg";
 import BookOpened from "./images/book-opened.svg";
 import Curve2 from "./images/curve-2.svg";
 import Curve1 from "./images/curve-1.svg";
-import Navbar from './Navbar';
-import Microphone from './images/microphone-solid.svg';
+
+const readOutLoud = (message) => {
+  const speech = new SpeechSynthesisUtterance();
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  speech.text = message;
+  window.speechSynthesis.speak(speech);
+}
 
 function App() {
-
+  const [transcript, setTranscript] = useState();
+  const content = document.querySelector('.content');
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.continuous = false;
+  recognition.lang = 'en-US';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
   
-  const [talk, setTalk] = useState(false);
+  useEffect(() => {
+
+    recognition.onstart = () => {
+      console.log("voice activated");
+  }
+
+  recognition.onresult = function(event) {
+      console.log(event);
+      const index = event.resultIndex;
+      setTranscript(event.results[index][0].transcript);
+      console.log(transcript);
+      readOutLoud(transcript);
+  }
+  recognition.onnomatch = function(event) {
+      content.innerHTML = 'I didnt recognize that';
+    }
+    recognition.onerror = function(event) {
+      content.innerHTML = 'Error occurred: ' + event.error;
+    }
+
+  });
+
   return (
     
     <div>
@@ -22,7 +58,21 @@ function App() {
 
     <div className ="container home-img">
 
-    <Navbar /> 
+    <div className = "nav-wrapper">
+        <div className = "nav padding">
+            <div className = "logo">
+              Notifly
+            </div>
+            <div className = "nav-section">
+              <ul className = "nav-2">
+                <li><a href = "#">Home</a></li>
+                <li><a href = "#about">About</a></li>
+
+                <li><a href = "#">Your Files</a></li>
+              </ul>
+            </div>
+        </div>  
+    </div>
 
     <img className = "img-left" src = {ImgLeft} alt = "reading"></img>
     <img className = "img-right"  src = {ImgRight} alt = "books"></img>
@@ -38,7 +88,6 @@ function App() {
       <p>Notifly is a website that aims to improve the student's learning experience, whether in a traditional or virtual setting, by providing a tool for students to use.
         Instead of frantically writing down all notes in a lecture, students can focus less on "copying what is being said" and more time understanding content.
       </p>
-
     </div>
     <div className = "section">
       <img src= {Mission} alt = "mission"></img>
@@ -65,26 +114,25 @@ function App() {
 
   <div className = "card-section">
     <div className = "card">
-      <img src = {BookOpened}></img>
+      <img src = {BookOpened} alt = "book"></img>
       <p>Students must enable the microphone on this website. Then, the website will be able to record information about the lecture, producing a transcript.</p>
     </div>
     <div className = "card">
-      <img src = {BookOpened}></img>
+      <img src = {BookOpened} alt = "book"></img>
       <p>Then, students can save, edit or delete the file if they want. There will also be an option to hear siri state the information clearly.  </p>
     </div>
     <div className = "card">
-      <img src = {BookOpened}></img>
+      <img src = {BookOpened} alt = "book"></img>
       <p> They can access these materials/notes whenever they see fit- whether its late night studying, or reviewing for a nearby test.</p>
     </div>
   </div>
 
   <div className = "convert">
-    <h1>Convert Here:</h1>
-    {/* <button onClick = {isOn}><img src = {Microphone} alt = "microphone"></img>Speak Now</button> */}
-    <div>
-      {talk ? (<h1>Listening...</h1>) : (<h1>Not Recording</h1>)}
+    <div className = "convert-inner">
+      <h1>Convert Here:</h1>
+      <button onClick = {() => {recognition.start()}} class ="talk">speak</button>
     </div>
-
+    <textarea className = "content" placeholder = "Record Now!">{transcript}</textarea>
   </div>
 
 
@@ -107,8 +155,8 @@ function App() {
       <div className = "footer-section-1 list-flex">
         <li><a href = "index.html">Home</a></li>
         <li><a href = "about.html">About</a></li>
-        <li><a href = "FAQ.html">Files</a></li>
-        <li><a href = "contact.html">Convert</a></li>
+        <li><a href = "Files.html">Files</a></li>
+        <li><a href = "Convert.html">Convert</a></li>
       </div>
       </div>
     </div>
